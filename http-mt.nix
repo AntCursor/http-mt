e6f5fe9553576399defbdcea7a2770045d36f6ab
AntCursor <antcursor@gmail.com>
2026-01-31 22:17:12 -0300

    chore!: basic nix package file

---
+++ http-mt.nix
@@ -0,0 +1,22 @@
+{pkgs, stdenv, lib, ...}: 
+  let 
+    buildDeps = with pkgs; [gcc];
+
+    fs = lib.fileset;
+    sourceFiles = fs.difference
+      (fs.gitTracked ./.)
+      (fs.fileFilter (file: file.hasExt "nix") ./.);
+  in
+  fs.trace sourceFiles
+
+  stdenv.mkDerivation {
+    pname = "http-mt"; 
+    version = "0.1.0";
+
+    src = fs.toSource {
+      root = ./.;
+      fileset = sourceFiles;
+    };
+
+    buildInputs = buildDeps;
+  } 
